6f70ed5c1b27abbcf64a32684f05821661019d69
woodonghun <ehcldnehd@naver.com>
2025-10-02 10:56:04 +0900

    주석 추가

--- routes/users.py
+++ routes/users.py
@@ -15,6 +15,7 @@ def list_users(event):
     return response.ok(data)
 
 def login(event,api_url:str):
+    print("login start")
     body = json.loads(event.get("body") or "{}")
     login_id = body.get("login_id")
     password = body.get("password")
@@ -23,7 +24,10 @@ def login(event,api_url:str):
     url = f"{api_url}/api/user/login/"
     payload = {"login_id":login_id,"password":password}
 
+    print("login request to shop start")
     res = requests.post(url=url, json=payload)
+    print("login request to shop end")
+
     data = res.json()
 
     # result_code 실패
@@ -34,11 +38,13 @@ def login(event,api_url:str):
     refresh = data.get("refresh")
     user_id = get_user_from_token(access)
     register_user(refresh=refresh, user_id=user_id)
+
     cleanup_dynamodb_user()
+    print("login end")
     return response.ok(data=data)
 
 def register_user(refresh, user_id)->bool:
-
+    print("register user start")
     # dynamodb 에 기존 데이터 존재하는지 확인
     resp = dynamodb.get_item(
         TableName=TABLE_NAME,
@@ -61,10 +67,12 @@ def register_user(refresh, user_id)->bool:
         TableName=TABLE_NAME,
         Item=item                           
     )
+    print("register user end")
     return True
     
 
 def cleanup_dynamodb_user():
+    print("cleanup user start")
     # 1. 테이블 전체 스캔
     response = dynamodb.scan(TableName=TABLE_NAME)
     items = response.get("Items", [])
@@ -85,3 +93,5 @@ def cleanup_dynamodb_user():
                 Key={"User": {"S": str(user_id)}}
             )
             print(f"deleted_user : {user_id}")
+    print("cleanup user end")
+    
